9e6b4e35a7a4cba6fd2bb752254609d930df5277
Peter Li <pl.climbs@gmail.com>
2024-04-17 01:58:09 -0700

    Add HTMX for reactivity with a simple counter example

--- main.go
+++ main.go
@@ -9,11 +9,56 @@ import (
 	"github.com/peteli3/personal-v1/components"
 )
 
+var counters components.Counters
+
+func doHTMXGet(w http.ResponseWriter, r *http.Request) {
+	components.HTMXpage(counters).Render(r.Context(), w)
+}
+
+func doHTMXIncTemplPost(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	if r.Form.Has("incTempl") {
+		counters.Templ++
+	}
+	// use Redirect instead of GET to implement PRG pattern
+	// which prevents unwanted form submission when user refreshes a POST
+	http.Redirect(w, r, "/htmx", http.StatusSeeOther)
+}
+
+func doHTMXIncHTMXPost(w http.ResponseWriter, r *http.Request) {
+	counters.HTMX++
+	http.Redirect(w, r, "/htmx/incHTMX/success", http.StatusSeeOther)
+}
+
+func doHTMXIncHTMXSuccess(w http.ResponseWriter, r *http.Request) {
+	components.HTMXCounter(counters).Render(r.Context(), w)
+}
+
 func main() {
 	http.Handle("/", templ.Handler(components.Homepage()))
-	http.Handle("/htmx", templ.Handler(components.HTMXpage()))
+
+	// htmx playground
+	http.HandleFunc("/htmx", func(w http.ResponseWriter, r *http.Request) {
+		doHTMXGet(w, r)
+	})
+	http.HandleFunc("/htmx/incTmpl", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+			doHTMXIncTemplPost(w, r)
+		}
+	})
+	http.HandleFunc("/htmx/incHTMX", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+			doHTMXIncHTMXPost(w, r)
+		}
+	})
+	http.HandleFunc("/htmx/incHTMX/success", func(w http.ResponseWriter, r *http.Request) {
+		doHTMXIncHTMXSuccess(w, r)
+	})
+
+	// daisyui playground
 	http.Handle("/daisyui", templ.Handler(components.DaisyUIpage()))
 
+	// static content
 	http.Handle("/assets/", http.StripPrefix("/assets", http.FileServer(http.Dir("./assets"))))
 	http.Handle("/css/", http.StripPrefix("/css", http.FileServer(http.Dir("./css"))))
 
